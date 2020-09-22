a833487f1d1b645645e4166085024c8f7af76ed1
Edzard Ihmels <ei@move-elevator.de>
2020-09-22 09:27:07 +0200

    phpstan changes

--- src/RawJsonItemPriceOverviewApplication.php
+++ src/RawJsonItemPriceOverviewApplication.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace EdzardIhmels\PriceOverview;
 
-use EdzardIhmels\PriceOverView\Client\SteamClient;
+use EdzardIhmels\PriceOverview\Client\SteamClient;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class RawJsonItemPriceOverviewApplication
